9b3f90855dd07e22f7b03faad88336093cf4453f
Minyoung <alsendrha@naver.com>
2025-01-17 11:32:41 +0900

    간격 수정

--- src/components/section02/ProfileText.tsx
+++ src/components/section02/ProfileText.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProfileText = () => {
   return (
-    <div className="p-2 text-white w-full pr-20 max-[1410px]:pr-10 max-[1000px]:px-32 max-[740px]:px-10 max-[1000px]:mt-5 transformation flex items-center justify-start">
+    <div className="p-2 text-white w-full pr-20 max-[1410px]:pr-10 max-[1000px]:px-32 max-[740px]:px-10 max-[1000px]:mt-5 max-[1000px]:mb-20 transformation flex items-center justify-start">
       <div className="w-[80%] max-[1410px]:w-full">
         <p className="text-4xl font-bold max-[1000px]:text-2xl transformation">
           안녕하세요!
