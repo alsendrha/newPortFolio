5965ccb6c564a85f976ab527bc801267e39a182b
Minyoung <alsendrha@naver.com>
2025-01-17 16:35:13 +0900

    프로젝트 디테일 오버뷰 글자 크기 수정

--- src/components/project/Overview.tsx
+++ src/components/project/Overview.tsx
@@ -8,8 +8,12 @@ const Overview = ({ project }: OverviewProps) => {
   return (
     <div className="text-white w-full flex items-center justify-center p-[2vw]">
       <div className="w-full flex flex-col bg-gradient-to-l from-[#224e35] to-[#111111] p-[2vw] gap-[4vw] items-center justify-center rounded-xl">
-        <p className="text-[2.5vw] font-bold">overview</p>
-        <p className="px-[5vw] text-[1.3vw]">{project?.overview}</p>
+        <p className="text-[3vw] max-[1000px]:text-[5vw] font-bold transformation">
+          overview
+        </p>
+        <p className="px-[5vw] text-[1.4vw] max-[1000px]:text-[2vw] transformation">
+          {project?.overview}
+        </p>
       </div>
     </div>
   );
