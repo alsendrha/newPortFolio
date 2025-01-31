9a6e0f41e070850635d53d32e24e44a043fa2bf3
Minyoung <alsendrha@naver.com>
2025-01-31 14:25:23 +0900

    프로젝트 표지 수정

--- src/components/section05/listitem/ListItem.tsx
+++ src/components/section05/listitem/ListItem.tsx
@@ -29,7 +29,7 @@ const ListItem = ({ data }: ListItemProps) => {
         }}
       >
         <ListItemSkills data={data} />
-        <div className="absolute text-white bottom-[5vw] max-[1000px]:bottom-[8vw] left-0 z-10 w-full flex flex-col justify-center items-center  opacity-0 group-hover:opacity-100 transition-all duration-[.5s] ease-out">
+        <div className="absolute text-white bottom-[5vw] max-[1000px]:bottom-[15vw] left-0 z-10 w-full flex flex-col justify-center items-center  opacity-0 group-hover:opacity-100 transition-all duration-[.5s] ease-out">
           <p className="font-bold text-[1.2vw] max-[1000px]:text-[2vw]">
             기여도
           </p>
@@ -37,7 +37,7 @@ const ListItem = ({ data }: ListItemProps) => {
             {data.participation}
           </p>
         </div>
-        <div className="text-white w-full h-full flex flex-col pt-[6vw] items-center absolute top-0 left-0 bg-opacity-0 group-hover:bg-opacity-100 group-hover:bg-[rgba(0,0,0,0.5)]">
+        <div className="text-white w-full h-full flex flex-col pt-[6vw] max-[1000px]:pt-[10vw] items-center absolute top-0 left-0 bg-opacity-0 group-hover:bg-opacity-100 group-hover:bg-[rgba(0,0,0,0.5)]">
           <ListItemTitle data={data} />
           <div className="w-0 my-[5vw] group-hover:my-[0.5vw] flex items-center justify-center opacity-0 group-hover:opacity-100 group-hover:w-[50%] h-[0.1vw] bg-white transform transition-all duration-[0.5s] ease-out" />
           <ListBottomText data={data} />
